d9b1665bf895a5c08914a9a69c668e3a64ba813b
Gabriel Leal <gabrielleao.mb@gmail.com>
2020-08-24 15:53:01 -0300

    Added some validations

--- resources/js/validation/Validator.js
+++ resources/js/validation/Validator.js
@@ -4,9 +4,17 @@ export const minLength = (value = '', min, message) => value.length >= min || (m
 
 export const maxLength = (value = '', max, message) => value.length <= max || (message ?? `Maximo de ${max} caracteres.`);
 
-export const required = (value, message)  => !!value || (message ?? 'Campo obrigat�rio.');
+export const required = (value, message)  => !!value || (message ?? 'Campo obrigatório.');
 
-export const number = (value, message) => !isNaN(value) || (message ?? 'Apenas numeros.')
+export const number = (value, message) => !isNaN(value) || (message ?? 'Apenas numeros.');
+
+export const equals = (value, value2, message) => value === value2 || (message ?? 'Valores não iguais.');
+
+export const email = (value, message) => {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(value) || (message ?? 'Email inválido');
+};
+
 
 
 export const validate = (validations = []) => {
